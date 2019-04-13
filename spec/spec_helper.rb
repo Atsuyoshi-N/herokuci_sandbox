b5c7c48b605cfcc68bc83517d1c3afa41f94cc38
Atsuyoshi-N <ash.sit.you7@ezweb.ne.jp>
2019-04-13 14:34:19 +0900

    fix option to use headless

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -98,15 +98,15 @@ RSpec.configure do |config|
 end
 
 Capybara.configure do |capybara_config|
-  capybara_config.default_driver = :selenium_chrome
+  capybara_config.default_driver = :selenium_chrome_headless
   capybara_config.default_max_wait_time = 10
 end
 
-Capybara.register_driver :selenium_chrome do |app|
+Capybara.register_driver :selenium_chrome_headless do |app|
   options = Selenium::WebDriver::Chrome::Options.new
-  options.add_argument('headless')
+  options.add_argument('--headless')
   options.add_argument('--disable-gpu')
   Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
 end
 
-Capybara.javascript_driver = :selenium_chrome
+Capybara.javascript_driver = :selenium_chrome_headless
