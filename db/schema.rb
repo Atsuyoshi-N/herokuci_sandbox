a5b131bff6cd050434b7f135b32c9fb44b330500
Atsuyoshi-N <ash.sit.you7@ezweb.ne.jp>
2019-04-13 14:24:24 +0900

    use postgresql in local test env

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_04_06_133647) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "messages", force: :cascade do |t|
     t.string "title"
     t.text "body"
