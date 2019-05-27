8a575b3b339bb9487bc21054905b46dbeb218b0c
Atsuyoshi-N <ash.sit.you7@ezweb.ne.jp>
2019-05-27 20:10:45 +0900

    fix webdriver version

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -96,12 +96,13 @@ RSpec.configure do |config|
   Kernel.srand config.seed
 =end
 end
+Webdrivers::Chromedriver.version = '2.46'
+
 chrome_bin = ENV.fetch('GOOGLE_CHROME_SHIM', nil)
 
 options = {}
 options[:args] = ['headless', 'disable-gpu', 'window-size=1280,1024']
 options[:binary] = chrome_bin if chrome_bin
-
 Capybara.register_driver :headless_chrome do |app|
   Capybara::Selenium::Driver.new(app,
                                  browser: :chrome,
